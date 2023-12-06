cd5efec30c310f25943ed597375c2b5229d8d362
Victor Villazón <victorzn23@gmail.com>
2023-12-05 21:58:47 -0500

    javascript modules: reto 10

---
+++ 04-javascript-retos/reto02-maquina-de-cambio/main.js
@@ -0,0 +1,7 @@
+import getCoins from "./solucion.js";
+
+console.log(getCoins(51)) // [1, 0, 0, 0, 0, 1] -> una moneda de 1 céntimo y otra de 50 céntimos
+console.log(getCoins(3)) // [1, 1, 0, 0, 0, 0] -> una moneda de 1 céntimo y otra de 2
+console.log(getCoins(5)) // [0, 0, 1, 0, 0, 0] -> una moneda de 5 céntimos
+console.log(getCoins(16)) // [1, 0, 1, 1, 0, 0] -> una moneda de 1 céntimo, una de 5 y una de 10
+console.log(getCoins(100)) // [0, 0, 0, 0, 0, 2] -> dos monedas de 50 céntimos
