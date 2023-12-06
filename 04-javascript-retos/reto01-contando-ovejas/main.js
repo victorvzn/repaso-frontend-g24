6dcc3b53846bd7f0b32e7a55339b2382b8b42a76
Victor Villazón <victorzn23@gmail.com>
2023-12-05 20:18:45 -0500

    javascript modules

---
+++ 04-javascript-retos/reto01-contando-ovejas/main.js
@@ -0,0 +1,14 @@
+import contarOvejas from './solucion.js'
+
+const ovejas = [
+  { name: 'Noa', color: 'azul' },
+  { name: 'Euge', color: 'rojo' },
+  { name: 'Navidad', color: 'rojo' },
+  { name: 'Ki Na Ma', color: 'rojo'},
+  { name: 'AAAAAaaaaa', color: 'rojo' },
+  { name: 'Nnnnnnnn', color: 'rojo'}
+]
+
+console.log(
+  contarOvejas(ovejas)
+)
