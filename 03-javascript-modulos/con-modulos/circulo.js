0c4ee358f61d6502ba1e70b37b2ce7032c19346b
Victor Villazón <victorzn23@gmail.com>
2023-12-05 20:00:25 -0500

    javascript modules

---
+++ 03-javascript-modulos/con-modulos/circulo.js
@@ -0,0 +1,9 @@
+const PI = Math.PI
+
+// EXPORTACION POR DEFECTO (DEFAULT)
+
+function area(radio) {
+  return PI * radio * radio
+}
+
+export default area
--- 03-javascript-modulos/con-modulos/main.js
+++ 03-javascript-modulos/con-modulos/main.js
@@ -1,6 +1,8 @@
 console.log('Hola Javascript!')
 import { suma, resta, division, multiplicacion, PI as PI2 } from './calculadora.js'
 
+import area from './circulo.js'
+
 console.log(suma(4, 9))
 console.log(resta(4, 9))
 console.log(multiplicacion(4, 9))
@@ -10,3 +12,6 @@ console.log(division(4, 9))
 console.log(PI2)
 
 // saludo() // Uncaught SyntaxError
+
+
+console.log(area(5))
